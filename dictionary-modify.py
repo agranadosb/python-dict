6042667ad27f732904e18a7d406abe6d733bef63
Alejandro <agranados@visiion.net>
2019-02-15 19:02:12 +0100

    Método para cambiar masivamente un json

--- dictionary-modify.py
+++ dictionary-modify.py
@@ -2,12 +2,15 @@
 
 class ChangesDicts():
     def change_dict(self, dicti, key, value): 
-        """ Function that changes the value of a key given of a dictionary or json
-            :param dicti: Dict to change
-            :param key: Key of the value to change
-            :param value: Value to be changed
-            :return: Dictionary with the value changed, if the key isn't on the dictionary,
-                     the dictionary won't be changed """
+        """Function that changes the value of a key given of a dictionary or json
+
+        :param dicti: Dict to change
+    
+        :param key: Key of the value to change
+    
+        :param value: Value to be changed
+    
+        :return: Dictionary with the value changed, if the key isn't on the dictionary, the dictionary won't be changed """
         if isinstance(dicti,(list,tuple)):
             res = []
             for subItem in dicti:
@@ -25,3 +28,29 @@ class ChangesDicts():
             return dicti
         elif isinstance(dicti, str):
             return dicti
+
+    def changes_dict(self, tree, change):
+        """Function that changes the values of a json with the keys given
+
+        :param tree: Json to be changed
+
+        :param change: Dictionary with the keys to be changed and the new values: {field1: value1, field2: value2,..., fieldN: valueN}"""
+        if isinstance(tree,(list,tuple)):
+            res = []
+            for subItem in tree:
+                result = self.changes_dict(subItem, change)
+                res.append(result)
+            return result
+        elif isinstance(tree,dict):
+            for nodeName in tree.keys():
+                subTree = tree[nodeName]
+                if nodeName in list(change.keys()):
+                    tree[nodeName] = {'value': str(change[nodeName])}
+                    change.pop(nodeName)
+                    if not change:
+                        break
+                else:
+                    tree[nodeName] = self.changes_dict(subTree, change)
+            return tree
+        elif isinstance(tree, str):
+            return tree
